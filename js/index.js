21a22f2f74e05ff65ee6960c8786303c5ccc3984
Ross Malcolm <rossmalc75@gmail.com>
2021-04-12 16:21:44 -0400

    Manage page almost completed

---
+++ js/index.js
@@ -0,0 +1,28 @@
+$.ajax({
+    url: "https://cp465-my-league.herokuapp.com/api/leagues",  
+    type: "GET",
+    success:function(result){
+        var data = JSON.parse(result);     
+        console.log(data);
+        for (i in data){
+            var row = data[i]
+            var leagueRow = document.createElement('tr');
+            leagueRow.style.cursor = 'pointer';
+
+            var createClickHandler = function(rowParam) {
+                return function() {
+                    sessionStorage.setItem('leagueID', rowParam.league_id);
+                    window.location.replace("./leaderboard.html");
+                };
+            };
+            leagueRow.innerHTML="<td>"+row.name+"</td>";
+            console.log(leagueRow);
+            leagueRow.onclick = createClickHandler(row);
+            $('#leagueTable tr:last').after(leagueRow);
+        }
+        },
+    error:function(xhr, status, error) {
+        var errorMessage = xhr.status + ': ' + xhr.statusText
+        alert('Error - ' + errorMessage);
+        }
+    });
